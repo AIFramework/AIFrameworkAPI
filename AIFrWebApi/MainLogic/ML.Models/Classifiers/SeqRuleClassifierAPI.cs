cda23daf6e116b95f5a23d04acbad0ad64f9768d
Zahar A. Ponimash <ponimashz@mail.ru>
2022-12-24 23:22:24 +0300

    Основная логика работы

---
+++ AIFrWebApi/MainLogic/ML.Models/Classifiers/SeqRuleClassifierAPI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainLogic.ML.Models.Classifiers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public class SeqRuleClassifierAPI
+    {
+    }
+}
---
+++ AIFrWebApi/MainLogic/ML.Models/Classifiers/TextRuleClassifierAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainLogic.ML.Models.Classifiers
+{
+    /// <summary>
+    /// Апи для классификатора текстов на правилах
+    /// </summary>
+    public class TextRuleClassifierAPI
+    {
+    }
+}
