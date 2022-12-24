b221e3bfa15e1a5a0aeb2933da6f0ad69c859438
Zahar A. Ponimash <ponimashz@mail.ru>
2022-12-24 23:24:34 +0300

    .

--- AIFrWebApi/MainLogic/ML.Models/Classifiers/SeqRuleClassifierAPI.cs
+++ AIFrWebApi/MainLogic/ML.Models/Classifiers/SeqRuleClassifierAPI.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace MainLogic.ML.Models.Classifiers
 {
     /// <summary>
-    /// 
+    /// Апи классификатора последовательностей на базе правил 
     /// </summary>
     [Serializable]
     public class SeqRuleClassifierAPI
